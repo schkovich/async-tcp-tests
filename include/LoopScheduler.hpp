632162ded9ce88861393aadd11e8d2a3f7cbf12c
Goran Mišković <schkovich@users.noreply.github.com>
2025-08-16 21:34:22 +0200

    [feat]: Switch LoopScheduler key type from string to int for efficiency. - Changed entry_key type from std::string to uint8_t for more rational and efficient key usage. - Updated all relevant method signatures and internal logic in LoopScheduler to use uint8_t keys. - Removed unnecessary string includes and simplified code for better performance and clarity.
    Signed-off-by: Goran Mišković <schkovich@users.noreply.github.com>

--- include/LoopScheduler.hpp
+++ include/LoopScheduler.hpp
@@ -4,13 +4,12 @@
 #pragma once
 
 #include <cstdint>
-#include <string>
 #include <unordered_map>
 
 namespace e5 {
 
     using namespace std;
-    using entry_key = std::string;
+    using entry_key = uint8_t;
 
     class LoopScheduler {
         struct Entry {
@@ -23,8 +22,8 @@ namespace e5 {
     public:
         LoopScheduler() = default;
         ~LoopScheduler() = default;
-        void setEntry(const std::string &key, uint32_t interval);
-        bool timeToRun(const std::string &key);
+        void setEntry(const uint8_t &key, uint32_t interval);
+        bool timeToRun(const uint8_t &key);
     };
 
 } // namespace e5
--- src/LoopScheduler.cpp
+++ src/LoopScheduler.cpp
@@ -5,20 +5,21 @@
 #include "LoopScheduler.hpp"
 
 namespace e5 {
-    void LoopScheduler::setEntry(const std::string& key, uint32_t interval) {
-        auto result = loop_tasks.try_emplace(key, Entry{interval, 0});
-        if (!result.second) {
+    void LoopScheduler::setEntry(const uint8_t &key,
+                                 const uint32_t interval) {
+        if (auto [fst, snd] = loop_tasks.try_emplace(
+            key, Entry{interval, 0});
+            !snd) {
             // If the key already exists, update the interval
-            result.first->second.interval = interval;
-            result.first->second.counter = 0; // Reset counter for existing entry
+            fst->second.interval = interval;
+            fst->second.counter = 0; // Reset counter for existing entry
         }
     }
-    bool LoopScheduler::timeToRun(const std::string& key) {
-        auto it = loop_tasks.find(key);
-        if (it != loop_tasks.end()) {
+    bool LoopScheduler::timeToRun(const uint8_t &key) {
+        if (const auto it = loop_tasks.find(key); it != loop_tasks.end()) {
             if (it->second.counter >= it->second.interval) {
                 it->second.counter = 0; // Reset counter after running
-                return true; // Time to run the task
+                return true;            // Time to run the task
             }
             it->second.counter++;
         }
